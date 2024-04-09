cf43099fa676bbb94df2a335ba49d50ee33ec784
Fabra <gfabra@insgabrielamistral.cat>
2024-04-09 09:47:59 +0200

    Classe KeyboardExample creat

--- src/main/java/org/example/KeyboardExample.java
+++ src/main/java/org/example/KeyboardExample.java
@@ -1,4 +1,47 @@
 package org.example;
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class KeyboardExample {
+/**
+ * Classe per llegir el teclat de l'usuari
+ */
+public class KeyboardExample extends JPanel {
+    /**
+     * Constructor,
+     */
+    public KeyboardExample() {
+        KeyListener listener = new MyKeyListener();
+        addKeyListener(listener);
+        //Permet llegir el teclat de l'usuari
+        setFocusable(true);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Mini Tennis");
+        KeyboardExample keyboardExample = new KeyboardExample();
+        frame.add(keyboardExample);
+        frame.setSize(200, 200);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /**
+     * Mostra la tecla presionada
+     */
+    public class MyKeyListener implements KeyListener {
+        @Override
+        public void keyTyped(KeyEvent ke) {
+        }
+
+        @Override
+        public void keyPressed(KeyEvent ke) {
+            System.out.println("keyPressed="+KeyEvent.getKeyText(ke.getKeyCode()));
+        }
+
+        @Override
+        public void keyReleased(KeyEvent ke) {
+            System.out.println("keyReleased="+KeyEvent.getKeyText(ke.getKeyCode()));
+        }
+    }
 }
